eb5f32085a10d8cb3cd651e81e4bd8eb63f56fad
vagrant <vagrant@homestead>
2017-01-06 18:29:22 +0000

    add real teachers name

--- database/seeds/TeacherSeeder.php
+++ database/seeds/TeacherSeeder.php
@@ -12,6 +12,13 @@ class TeacherSeeder extends Seeder
      */
     public function run()
     {
-        $teachers = factory(App\Teacher::class, 3)->create();
+    	$teachers = [
+			'張慶齡','許書齊','江彩怡','李宜芳','曾如','李孟珊','張庭寧','陳冠宇','吳崇岳','劉銘婠','鐘楚紅','陳淑貞','林牧音',
+			'陳俊延','陳惠玲','張浩堂','謝耀霆','羅美雪','陳彭建中','楊惠閔','林桓妃','吳巧茹','林青良','董蘭英','曾志全','柯伊綺',
+			'賴仲軒','黃森簇','黃承翰'
+		];
+        foreach ($teachers as $teacher) {
+        	Teacher::create(['name'=>$teacher]);
+        }
     }
 }
